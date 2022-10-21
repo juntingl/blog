115aa091bf479131b06a6f8fcab7de2c81e4691d
Junting <342766475@qq.com>
2022-10-21 10:14:43 +0800

    ci: modify deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -20,6 +20,6 @@ git commit -m 'ci: deploy'
 # git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:JuntingLiu/blog.git master:gh-pages
+git push -f git@github.com:JuntingLiu/blog.git main:gh-pages
 
 cd -
